0b351b6b1dfe671534a060aafef7953f33f1b600
JoshuaMaurer <maurer.joshua@gmail.com>
2023-03-28 17:44:35 -0700

    added a cleaned up function. Commented out so as not to be reviewed with initial assessment since submitted later.

--- code-challenges.test.js
+++ code-challenges.test.js
@@ -14,14 +14,14 @@
 
 // a) Create a test with expect statements using the variables provided.
 
-// const secretCodeWord1 = "Lackadaisical"
-// // Expected output: "L4ck4d41s1c4l"
-// const secretCodeWord2 = "Gobbledygook"
-// // Expected output: "G0bbl3dyg00k"
-// const secretCodeWord3 = "Eccentric"
-// // Expected output: "3cc3ntr1c"
-// const secretCodeWord4 = "EeEe"
-// const secretCodeWord5 = "Supercalifragilisticexpialidocious"
+const secretCodeWord1 = "Lackadaisical"
+// Expected output: "L4ck4d41s1c4l"
+const secretCodeWord2 = "Gobbledygook"
+// Expected output: "G0bbl3dyg00k"
+const secretCodeWord3 = "Eccentric"
+// Expected output: "3cc3ntr1c"
+const secretCodeWord4 = "EeEe"
+const secretCodeWord5 = "Supercalifragilisticexpialidocious"
 
 // input: a string
 
@@ -31,21 +31,21 @@
 // Create a function called superSecretCodingMachine that takes a string as a parameter. Use .replaceAll() to replace all instances of the searched letter with the regex search pattern. Use /g to search all occurences of the letter in the string and /i to be case insensitive, then use a comma to separate the search pattern from the replacement value and add corresponding replacement value. Repeat this .replaceALL pattern three additional times, changing to the required corresponding searched letter and its number replacement. Return in one line off the single passed through string parameter.
 
 // test:
-describe("superSecretCodingMachine", () => {
-    it("take in a string and returns a string with a coded message" , () => {
-        const secretCodeWord1 = "Lackadaisical"
-        const secretCodeWord2 = "Gobbledygook"
-        const secretCodeWord3 = "Eccentric"
-        const secretCodeWord4 = "EeEe"
-        const secretCodeWord5 = "Supercalifragilisticexpialidocious"
+// describe("superSecretCodingMachine", () => {
+//     it("take in a string and returns a string with a coded message" , () => {
+//         const secretCodeWord1 = "Lackadaisical"
+//         const secretCodeWord2 = "Gobbledygook"
+//         const secretCodeWord3 = "Eccentric"
+//         const secretCodeWord4 = "EeEe"
+//         const secretCodeWord5 = "Supercalifragilisticexpialidocious"
 
-        expect(superSecretCodingMachine(secretCodeWord1)).toEqual("L4ck4d41s1c4l"),
-        expect(superSecretCodingMachine(secretCodeWord2)).toEqual("G0bbl3dyg00k"),
-        expect(superSecretCodingMachine(secretCodeWord3)).toEqual("3cc3ntr1c"),
-        expect(superSecretCodingMachine(secretCodeWord4)).toEqual("3333"),
-        expect(superSecretCodingMachine(secretCodeWord5)).toEqual("S6p3rc4l1fr4g1l1st1c3xp14l1d0c106s")
-    })
-})
+//         expect(superSecretCodingMachine(secretCodeWord1)).toEqual("L4ck4d41s1c4l"),
+//         expect(superSecretCodingMachine(secretCodeWord2)).toEqual("G0bbl3dyg00k"),
+//         expect(superSecretCodingMachine(secretCodeWord3)).toEqual("3cc3ntr1c"),
+//         expect(superSecretCodingMachine(secretCodeWord4)).toEqual("3333"),
+//         expect(superSecretCodingMachine(secretCodeWord5)).toEqual("S6p3rc4l1fr4g1l1st1c3xp14l1d0c106s")
+//     })
+// })
 
 // jest-test-1:
 // FAIL  ./code-challenges.test.js
@@ -134,6 +134,26 @@ const superSecretCodingMachine = (string) => {
     return string.replaceAll(/a|e|E|i|o|u/gi, (matched) => superSecretCode[matched])
 }
 
+// This one below was done AFTER initial assessment submission. It has a couple additional things cleaned up. The one above passes but I was not happy with it. 
+
+// Additional comment from Tuesday night after looking at the code with fresh eyes for the Wednesday review. It's funny how simple things can look once you step away for a bit and come back with fresh eyes. I looked at it for about 2 min after class when putting the code up for review in the thread. I was able to see right away that to catch other capital letter edge cases, I needed to add the additional key:value pairs. Also that I did not need the caps E in the regex search since I was using the /i. I had looked at doing .toLowerCase after the string but then to reverse that involved a lot more code. So with all this being said, I think my favorite option here has changed back to my original 4 x .replaceAll funtion. Is there a best practice as to which one would be best? Thanks Gene I really appreciate everything your doing with going through all these assessments and your insight into them! 
+
+// const superSecretCodingMachine = (string) => {
+//     const superSecretCode = {
+//         "a": 4,
+//         "A": 4,
+//         "e": 3,
+//         "E": 3,
+//         "i": 1,
+//         "I": 1,
+//         "o": 0,
+//         "O": 0,
+//         "u": 6,
+//         "U": 6,
+//       };
+//     return string.replaceAll(/a|e|i|o|u/gi, (matched) => superSecretCode[matched])
+// }
+
 // --------------------2) Create a function that takes in an array of words and a single letter and returns an array of all the words containing that particular letter.
 
 // a) Create a test with expects statement using the variable provided.
@@ -155,21 +175,21 @@ const superSecretCodingMachine = (string) => {
 // .filter to create a shallow copy of the original array. Inside .filter, pass the value, then use value.toLowerCase to account for any capital letters. After the parens of the .toLowerCase() use .includes and pass the letter that is to be filtered.
 
 
-test:
-describe("valueContainsALetter", () => {
-    it("return an array of all the words containing that particular letter", () => {
-        const fruitArray = ["Mango", "Cherry", "Apricot", "Blueberry", "Peach", "Kiwi"]
-        const letterA = "a"
-        const letterE = "e"
-        const letterM = "m"
-        const letterY = "y"
+// test:
+// describe("valueContainsALetter", () => {
+//     it("return an array of all the words containing that particular letter", () => {
+//         const fruitArray = ["Mango", "Cherry", "Apricot", "Blueberry", "Peach", "Kiwi"]
+//         const letterA = "a"
+//         const letterE = "e"
+//         const letterM = "m"
+//         const letterY = "y"
 
-        expect(valueContainsALetter(fruitArray, letterA)).toEqual(["Mango", "Apricot", "Peach"])
-        expect(valueContainsALetter(fruitArray, letterE)).toEqual(["Cherry", "Blueberry", "Peach"])
-        expect(valueContainsALetter(fruitArray, letterM)).toEqual(["Mango"])
-        expect(valueContainsALetter(fruitArray, letterY)).toEqual(["Cherry", "Blueberry"])
-    })
-})
+//         expect(valueContainsALetter(fruitArray, letterA)).toEqual(["Mango", "Apricot", "Peach"])
+//         expect(valueContainsALetter(fruitArray, letterE)).toEqual(["Cherry", "Blueberry", "Peach"])
+//         expect(valueContainsALetter(fruitArray, letterM)).toEqual(["Mango"])
+//         expect(valueContainsALetter(fruitArray, letterY)).toEqual(["Cherry", "Blueberry"])
+//     })
+// })
 
 // jest-test-1:
 // FAIL  ./code-challenges.test.js
@@ -228,9 +248,9 @@ describe("valueContainsALetter", () => {
 
 // b) Create the function that makes the test pass.
 
-const valueContainsALetter = (array, letter) => {
-    return array.filter((value) => value.toLowerCase().includes(letter))
-}
+// const valueContainsALetter = (array, letter) => {
+//     return array.filter((value) => value.toLowerCase().includes(letter))
+// }
 
 // --------------------3) Create a function that takes in an array of 5 numbers and determines whether or not the array is a "full house". A full house is exactly one pair and one three of a kind.
 
@@ -255,25 +275,25 @@ const valueContainsALetter = (array, letter) => {
 // Create a function called checkIfTheHouseIsFull and pass the array containing 5 numbers as the parameter. Create a let variable inside of the function called arr and have it equal the original array that has been sorted using .sort() that will natively return the array values in ascending order. Using a conditional if/else statement, check the sorted values index location against each other using strict = (===). If the values of the 0 & 1 as well as the values of 2, 3, & 4 locations match, OR (||), the values of the 0, 1, & 2 as well as the values of 3 & 4 match, return true. Else return false.
 
 // test:
-describe("checkIfTheHouseIsFull", () => {
-    it('determine whether or not the array is a "full house, exactly one pair and one three of a kind"', () => {
-        const hand1 = [5, 5, 5, 3, 3]
-        const hand2 = [5, 5, 3, 3, 4]
-        const hand3 = [5, 5, 5, 5, 4]
-        const hand4 = [7, 2, 7, 2, 7]
-        const hand5 = [7, 2, 7, 2, 2]
-        const hand6 = ['A', 'K', 7, 5, 6]
-        const hand7 = ['A', 'A', 2, 'A', 2]
+// describe("checkIfTheHouseIsFull", () => {
+//     it('determine whether or not the array is a "full house, exactly one pair and one three of a kind"', () => {
+//         const hand1 = [5, 5, 5, 3, 3]
+//         const hand2 = [5, 5, 3, 3, 4]
+//         const hand3 = [5, 5, 5, 5, 4]
+//         const hand4 = [7, 2, 7, 2, 7]
+//         const hand5 = [7, 2, 7, 2, 2]
+//         const hand6 = ['A', 'K', 7, 5, 6]
+//         const hand7 = ['A', 'A', 2, 'A', 2]
 
-        expect(checkIfTheHouseIsFull(hand1)).toEqual(true)
-        expect(checkIfTheHouseIsFull(hand2)).toEqual(false)
-        expect(checkIfTheHouseIsFull(hand3)).toEqual(false)
-        expect(checkIfTheHouseIsFull(hand4)).toEqual(true)
-        expect(checkIfTheHouseIsFull(hand5)).toEqual(true)
-        expect(checkIfTheHouseIsFull(hand6)).toEqual(false)
-        expect(checkIfTheHouseIsFull(hand7)).toEqual(true)
-    })
-})
+//         expect(checkIfTheHouseIsFull(hand1)).toEqual(true)
+//         expect(checkIfTheHouseIsFull(hand2)).toEqual(false)
+//         expect(checkIfTheHouseIsFull(hand3)).toEqual(false)
+//         expect(checkIfTheHouseIsFull(hand4)).toEqual(true)
+//         expect(checkIfTheHouseIsFull(hand5)).toEqual(true)
+//         expect(checkIfTheHouseIsFull(hand6)).toEqual(false)
+//         expect(checkIfTheHouseIsFull(hand7)).toEqual(true)
+//     })
+// })
 
 // jest-test-1:
 // FAIL  ./code-challenges.test.js
@@ -352,9 +372,9 @@ describe("checkIfTheHouseIsFull", () => {
 //     a[0] === a[1] && a[0] === a[2] && a[3] === a[4] ? true : false) 
 // }
 
-checkIfTheHouseIsFull = (array) => {
-    const count = {}
-        array.map(value => count[value] = (count[value] || 0) + 1)
-        return Object.values(count).includes(2) && Object.values(count).includes(3)
-}
+// checkIfTheHouseIsFull = (array) => {
+//     const count = {}
+//         array.map(value => count[value] = (count[value] || 0) + 1)
+//         return Object.values(count).includes(2) && Object.values(count).includes(3)
+// }
 
